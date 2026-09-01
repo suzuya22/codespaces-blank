debc16a92ac28e35b261283020564e470b21c9ea
Juanp1103 <164225812+Juanp1103@users.noreply.github.com>
2026-09-01 02:49:01 +0000

    Cambios React Router

---
+++ src/Pages/registro/Registro.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import reactLogo from '../../assets/react.svg'
+import viteLogo from '../../assets/vite.svg'
+import heroImg from '../../assets/hero.png'
+import './Registro.css'
+
+function Registro() {
+  
+  return (
+    <>
+    <h1>Registro</h1>
+    <form action="">
+      <label htmlFor="">Nombre:
+      <input type="text" name="" id="" />
+      </label> <br />
+      <label htmlFor="">Apellido:
+      <input type="text" name="" id="" />
+      </label>
+    </form>
+    </>
+  )
+}
+
+export default Registro
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,22 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter,Routes, Route } from "react-router";
 import './index.css'
+import Creditos from "./pages/creditos/Creditos.jsx"
+import Galeria from "./pages/galeria/Galeria.jsx"
+import Home from './pages/home/Home.jsx'
+import Registro from './pages/registro/Registro.jsx'
+import Menu from "./components/ui/menu/Menu.jsx"
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <BrowserRouter>
+    <Menu />
+    <Routes>
+      <Route path="/creditos" element={<Creditos />} />
+      <Route path="/galeria" element={<Galeria />} />
+      <Route path="/registro" element={<Registro />} />
+      <Route path="/" element={<Home />} />
+    </Routes>
+  </BrowserRouter>,
 )
